157cee05e16897440d85750a04ed2e915f1a3d4c
stardisblue <stardisblue@gmail.com>
2014-04-04 10:54:04 +0200

    final

--- plg.myusergroupfield.php
+++ plg.myusergroupfield.php
@@ -7,7 +7,7 @@
 * @author CHEN Fati
 * @copyright (C) 2014
 * @license Limited http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU/GPL version 2
-* @final 1.0.0qsdfqsdfqsdfqsdfqdsfqsdf
+* @final 1.0.0
 */
 
 /** ensure this file is being included by a parent file */
@@ -17,6 +17,7 @@ global $_PLUGINS;
 $_PLUGINS->loadPluginGroup( 'user', array( (int) 1 ) );
 $_PLUGINS->registerUserFieldTypes( array( 'myusergroupfield' => 'CBfield_myusergroupfield' ) );
 $_PLUGINS->registerUserFieldParams();
+$_PLUGINS->registerFunction( 'onAfterUserRegistration', 'changeUser','CBfield_myusergroupfield' );
 
 class CBfield_myusergroupfield extends CBfield_counter {
 	/**
@@ -29,6 +30,27 @@ class CBfield_myusergroupfield extends CBfield_counter {
 	 * @param  int                   $list_compare_types   IF reason == 'search' : 0 : simple 'is' search, 1 : advanced search with modes, 2 : simple 'any' search
 	 * @return mixed                
 	 */
+	function changeUser(&$user){
+		global $_CB_framework, $ueConfig, $_CB_database;
+		
+		$oReturn = null;
+		$query = "SELECT params FROM #__comprofiler_fields WHERE type='mysqlfield'";
+		$_CB_database->setQuery($query);
+		$value = $_CB_database->loadResult();
+		
+		$params=preg_split('/field[^=]*=/i', $value, -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE);
+		$viewer =& JFactory::getUser();
+		
+		// Built MySQL Query
+		$query = "UPDATE #__user_usergroup_map SET group_id = (SELECT id FROM #__usergroups WHERE title=(SELECT " . $params[0] . " AS mysqlfield";
+		$query .= " FROM " . $params[1];
+		$query .= " WHERE " . $params[2].")) WHERE user_id =". $user->id." AND group_id=".$params[3];
+
+		$_CB_database->setQuery(str_replace("{USERID}",$user->id, str_replace("{VIEWERID}", $viewer->id, $query)));
+		$_CB_database->query();
+		return $oReturn;
+	}
+	
 	function getField( &$field, &$user, $output, $reason, $list_compare_types ) {
 		global $_CB_framework, $ueConfig, $_CB_database;
 
@@ -36,8 +58,8 @@ class CBfield_myusergroupfield extends CBfield_counter {
 		$fieldwhere = $field->params->get( 'fieldWhere', '' );
 		$fieldgroupid = $field->params->get( 'fieldGroupe','0');
 		$viewer =& JFactory::getUser();
-		
-	
+
+
 		// Built MySQL Query
 		$query = "UPDATE #__user_usergroup_map SET group_id = (SELECT id FROM #__usergroups WHERE title=(SELECT " . $field->params->get( 'fieldSelect', '' ) . " AS mysqlfield";
 		$query .= " FROM " . $field->params->get( 'fieldFrom', '' );
@@ -54,7 +76,7 @@ class CBfield_myusergroupfield extends CBfield_counter {
 			case 'rss':
 				$oReturn				=	null;
 				break;
-	
+
 			case 'htmledit':
 				// $oReturn				=	parent::getField( $field, $user, $output, $reason, $list_compare_types );
 				$oReturn				=	null;		//TBD for now no searches...not optimal in SQL anyway.
@@ -71,6 +93,5 @@ class CBfield_myusergroupfield extends CBfield_counter {
 				break;
 		}
 		return $oReturn;
-	}
 }//end of My User Group Field field
 ?>
