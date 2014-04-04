3a4b8c28eddaa23057144c593529313e1548b8b0
stardisblue <stardisblue@gmail.com>
2014-04-04 11:07:34 +0200

    correctif, accollade manquante ligne 85

--- plg.myusergroupfield.php
+++ plg.myusergroupfield.php
@@ -34,14 +34,15 @@ class CBfield_myusergroupfield extends CBfield_counter {
 		global $_CB_framework, $ueConfig, $_CB_database;
 		
 		$oReturn = null;
+		
 		$query = "SELECT params FROM #__comprofiler_fields WHERE type='mysqlfield'";
 		$_CB_database->setQuery($query);
 		$value = $_CB_database->loadResult();
-		
 		$params=preg_split('/field[^=]*=/i', $value, -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE);
+		
 		$viewer =& JFactory::getUser();
 		
-		// Built MySQL Query
+		// Built My User Field Query
 		$query = "UPDATE #__user_usergroup_map SET group_id = (SELECT id FROM #__usergroups WHERE title=(SELECT " . $params[0] . " AS mysqlfield";
 		$query .= " FROM " . $params[1];
 		$query .= " WHERE " . $params[2].")) WHERE user_id =". $user->id." AND group_id=".$params[3];
@@ -81,5 +82,6 @@ class CBfield_myusergroupfield extends CBfield_counter {
 				break;
 		}
 		return $oReturn;
+	}
 }//end of My User Group Field field
 ?>
